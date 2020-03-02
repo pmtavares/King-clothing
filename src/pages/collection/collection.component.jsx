d43c9b9344a806de1c8b6277a0def38c05affabf
pmtavares <pedrotavares41@gmail.com>
2020-03-02 22:52:31 +0000

    Added categories mapping and selector

---
+++ src/pages/collection/collection.component.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import CollectionItem from '../../components/collection-item/collection-item.component'
+import './collection.style.css';
+import {connect} from 'react-redux'
+import {selectCollection} from '../../redux/shop/shop.selector'
+
+const CollectionPage = ({collection}) => {
+    console.log(collection)
+    return(
+        <div className="collection-page">
+            <h2>CollectionPage</h2>
+        </div>
+    )
+}
+
+const mapStateToProps = (state, ownProps) => ({
+    collection: selectCollection(ownProps.match.params.collectionId)(state)
+})
+
+export default connect(mapStateToProps)(CollectionPage);
+
