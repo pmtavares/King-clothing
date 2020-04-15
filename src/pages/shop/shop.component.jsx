95bd43baa3474aa310e400b968b07d94ab7b7ca9
pmtavares <pedrotavares41@gmail.com>
2020-04-15 13:24:25 +0100

    High Order Component

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -5,10 +5,16 @@ import {Route} from 'react-router-dom';
 import CollectionPage from '../collection/collection.component'
 import {firestore, convertCollectionsSnapshotToMap} from '../../firebase/firebase.util';
 import {updateCollections} from '../../redux/shop/shop.actions'
+import WithSpinner from '../../components/with-spinner/with-spinner.component'
 
+const CollectionOverviewWithSpinner = WithSpinner(CollectionsOverview);
+const CollectionPageWithSpinner = WithSpinner(CollectionPage);
 
 class ShopPage extends React.Component {
 
+    state = {
+        loading: true
+    }
     unsubscribeFromSnapshot = null;
 
     componentDidMount()
@@ -19,15 +25,18 @@ class ShopPage extends React.Component {
         this.unsubscribeFromSnapshot = collectionRef.onSnapshot(async snapshot => {
             const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
             updateCollections(collectionsMap);
+            this.setState({loading: false});
         });
     }
     render(){
         const {match} = this.props;
-
+        const {loading} = this.state;
         return (
             <div className='shop-page'>
-                <Route exact path={`${match.path}`} component={CollectionsOverview}/>
-                <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
+                <Route exact path={`${match.path}`} 
+                    render={(props) => <CollectionOverviewWithSpinner isLoading={loading} {...props}/>}/>
+                <Route path={`${match.path}/:collectionId`} 
+                    render={(props)=><CollectionPageWithSpinner isLoading={loading} {...props} />} />
             </div>            
         );
     }
